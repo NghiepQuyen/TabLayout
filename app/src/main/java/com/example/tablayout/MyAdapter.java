73895411f06e00b8ca539b9675a97c507f92cce0
Quyen <quyen_it@mail.klb.com.tw>
2023-02-07 09:58:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tablayout/MyAdapter.java
@@ -0,0 +1,36 @@
+package com.example.tablayout;
+
+import android.content.Context;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyAdapter extends FragmentPagerAdapter {
+    Context context;
+    int totalTabs;
+    public MyAdapter(Context c, FragmentManager fm, int totalTabs) {
+        super(fm);
+        context = c;
+        this.totalTabs = totalTabs;
+    }
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                FootBall footballFragment = new FootBall();
+                return footballFragment;
+            case 1:
+                Cricket cricketFragment = new Cricket();
+                return cricketFragment;
+            case 2:
+                NBA nbaFragment = new NBA();
+                return nbaFragment;
+            default:
+                return null;
+        }
+    }
+    @Override
+    public int getCount() {
+        return totalTabs;
+    }
+}
